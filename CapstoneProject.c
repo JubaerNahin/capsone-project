097bf30a526b312272737884fea0ca7937e90a02
Jubaer Islam Nahin <105843586+JubaerNahin@users.noreply.github.com>
2022-12-07 01:49:18 +0600

    Update CapstoneProject.c

--- CapstoneProject.c
+++ CapstoneProject.c
@@ -13,13 +13,13 @@ void delete();
     int id;
     float cgpa;
 };
-struct student data;
+  struct student data;
 void main()
 {
 
   int choice;
-  while(choice!=5){
-
+  while(1){
+    system("cls");
     printf("\t\t\t\t=====Student Data Storing Management=====");
     printf("\n\n\n\t\t\t\t     1. Add Student Data\n");
     printf("\t\t\t\t     2. Students Data\n");
@@ -27,59 +27,48 @@ void main()
     printf("\t\t\t\t     4. Delete Data\n");
     printf("\t\t\t\t     5. Exit\n");
     printf("\t\t\t\t     __________________________\n");
-    printf("\t\t\t\t     Enter your choice:");
+    printf("\t\t\t\t     Enter your choice: ");
     scanf("%d",&choice);
+
 
     switch(choice){
        case 1:
-          system("cls");
+
           addstudent();
-          system("cls");
           break;
       case 2:
-          system("cls");
+
           studentrecord();
-          printf("\t\t\t\t press any key to exit....... \n");
-          getch();
-          system("cls");
           break;
       case 3:
-          system("cls");
+
           searchstudent();
-          printf("\t\t\t\t press any key to exit....... \n");
-          getch();
-          system("cls");
           break;
       case 4:
-          system("cls");
+
           delete();
-          printf("\t\t\t\t press any key to exit....... \n");
-          getch();
-          system("cls");
           break;
       case 5:
-          system("cls");
-          printf("Thank You for using this software.\n\n");
+
+          printf("\n\t\t\t\tThank You for using this software.\n\n");
           exit(0);
           break;
       default:
-          system("cls");
-          getch();
+
           printf("\n\t\t\t\tEnter a valid number\n\n");
-          printf("\t\t\t\tPress any key to continue........");
-          getch();
-          system("cls");
           break;
     }
+    printf("\n\t\t\t\tPress any key to continue......");
+    getch();
   }
-  getch();
+
 }
 
 void addstudent(){
 
   char next;
   FILE *fp;
-  
+
 do{
   system("cls");
   printf("\t\t\t\t=======Add Student Data=======\n\n\n");
@@ -111,10 +100,10 @@ do{
 }
 
 void studentrecord(){
-
+  system("cls");
   FILE *fp;
+  int count=0;
 
-  
   fp=fopen("Data.txt","r");
 
    printf("\t\t\t\t=======Students Record=======\n\n\n");
@@ -122,31 +111,43 @@ void studentrecord(){
   if(fp==NULL){
 
     printf("can't open file\n");
-    exit(0);
+    return;
   }
   else{
     printf("\t\t\t\tRecords :\n");
     printf("\t\t\t\t___________\n\n");
-  }
+
   while(fread(&data,sizeof(struct student),1,fp)){
     printf("\n\t\t\t\t Student Name : %s %s",data.first_name,data.last_name);
     printf("\n\t\t\t\t Id           : %d",data.id);
     printf("\n\t\t\t\t Cgpa         : %.2f",data.cgpa);
     printf("\n\t\t\t\t_________________________________\n");
+    count=1;
 
-  }
-  fclose(fp);
-  getch();
+   }
+   if(count==0){
+    printf("\t\t\t\tThere is no record\n");
+   }
+   fclose(fp);
+
+ }
 }
 
 void searchstudent(){
-  
+
+  system("cls");
   FILE *fp;
   int id,found=0;
 
   fp=fopen("Data.txt","r");
   printf("\t\t\t\t=======Search Record of a Student=======\n\n\n");
-  printf("\t\t\tEnter The id : ");
+   if(fp==NULL){
+
+    printf("can't open file\n");
+    return;
+  }
+  else{
+        printf("\t\t\tEnter The id : ");
 
   scanf("%d",&id);
 
@@ -161,15 +162,16 @@ void searchstudent(){
       printf("\n\t\t\t_____________________________________________\n");
     }
   }
-  if(!found){
+  if(found==0){
     printf("\n\t\t\tRecord not found\n");
   }
   fclose(fp);
-  getch();
+
+ }
 }
 
 void delete(){
-  
+  system("cls");
   FILE *fp, *fp1;
 
   int id,found=0;
@@ -177,12 +179,13 @@ void delete(){
   printf("\t\t\t\t========Delete students record=======\n\n\n");
   fp=fopen("Data.txt","r");
   fp1=fopen("temp.txt","w");
-  printf("\t\t\t\tEnter the id : ");
-  scanf("%d",&id);
+
   if(fp==NULL){
     printf("can't open file");
     exit(0);
   }
+   printf("\t\t\t\tEnter the id : ");
+   scanf("%d",&id);
 
   while(fread(&data,sizeof(struct student),1,fp)){
     if(data.id==id){
@@ -195,14 +198,14 @@ void delete(){
    fclose(fp);
    fclose(fp1);
 
-  if(!found){
+  if(found==0){
     printf("\n\t\t\t\tRecord not found\n");
   }
-  if(found){
+   else{
+    printf("\n\t\t\t\tRecord deleted successfully\n");
+
+  }
     remove("Data.txt");
     rename("temp.txt","Data.txt");
 
-    printf("\n\t\t\t\tRecord deleted successfully\n");
-  }
-   getch();
 }
